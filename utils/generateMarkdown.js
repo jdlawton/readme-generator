0b7c650b96def49c7cae471e7d4cf27559315c21
JoeL <jlawton1203@gmail.com>
2020-07-25 10:42:04 -0500

    continuing to test badging/license info

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,20 +3,30 @@
 const generateLicense = license => {
   switch (license) {
     case "MIT":
-      badge = "![MIT](https://img.shields.io/badge/license-MIT-brightgreen)";
-      return `This software is licensed under the [MIT license](https://choosealicense.com/licenses/mit/).`
+      return {
+        badge: "![MIT](https://img.shields.io/badge/license-MIT-brightgreen)",
+        description: "This software is licensed under the [MIT license](https://choosealicense.com/licenses/mit/)."
+      }
     case "GNU GPLv3":
-      badge = "![GNU GPLv3](https://img.shields.io/badge/license-GNU%20GPLv3-blue)";
-      return `This software is licensed under the [GNU GPLv3 license](https://choosealicense.com/licenses/gpl-3.0/).`
+      return {
+        badge: "![GNU GPLv3](https://img.shields.io/badge/license-GNU%20GPLv3-blue)",
+        description: "This software is licensed under the [GNU GPLv3 license](https://choosealicense.com/licenses/gpl-3.0/)."
+      }
     case "Mozilla 2.0":
-      badge = "![Mozilla 2.0](https://img.shields.io/badge/license-Mozilla%202.0-orange)";
-      return `This software is licensed under the [Mozilla Public License 2.0](https://choosealicense.com/licenses/mpl-2.0/).`
+      return {
+        badge: "![Mozilla 2.0](https://img.shields.io/badge/license-Mozilla%202.0-orange)",
+        description: "This software is licensed under the [Mozilla Public License 2.0](https://choosealicense.com/licenses/mpl-2.0/)."
+      }
     case "Apache 2.0":
-      badge = "![Apache 2.0](https://img.shields.io/badge/license-Apache%202.0-yellow)";
-      return `This software is licensed under the [Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/).`
+      return {
+        badge: "![Apache 2.0](https://img.shields.io/badge/license-Apache%202.0-yellow)",
+        description: "This software is licensed under the [Apache License 2.0](https://choosealicense.com/licenses/apache-2.0/)."
+      }
     default:
-      badge = "![Unlicensed](https://img.shields.io/badge/license-Unlicensed-red)";
-      return `This software is not available under any license and may not be copied, distributed, or modified.`
+      return {
+        badge: "![Unlicensed](https://img.shields.io/badge/license-Unlicensed-red)",
+        description: "This software is not available under any license and may not be copied, distributed, or modified."
+      }
   }
 }
 
@@ -25,12 +35,17 @@ const generateLicense = license => {
 function generateMarkdown(data) {
 
   const {title, description, installation, usage, license, contribute, tests, ...contact} = data;
+
+  //in order to include license info in two separate parts of the README (the badge at the top and the license description in the document body), i call generateLicense
+  //before I start creating the document, this will select the badge and description that corresponds with the user's choice and return an object with both values which can
+  //then be plugged into the README file in their appropriate places.
   const licenseInfo = generateLicense(license);
+  console.log(licenseInfo);
   //console.log(data);
   return `
   # ${title}
 
-  ${badge}
+  ${licenseInfo.badge}
 
   ## Description
   ${description}
@@ -51,7 +66,7 @@ function generateMarkdown(data) {
   ${usage}
 
   ## License
-  ${licenseInfo}
+  ${licenseInfo.description}
 
   ## Contribution
   ${contribute}
